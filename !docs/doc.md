f471a7d5bee15bb0f56fb0f1e0a8a955d95f68e2
Athari <athari.p@gmail.com>
2025-02-25 09:15:22 +0300

    @athari/recolor: WIP combining similar selectors from minimization.

---
+++ !docs/doc.md
@@ -0,0 +1,26 @@
+* postcss
+
+    * api
+        * https://postcss.org/api/
+        * https://astexplorer.net/
+        * https://postcss.org/docs/writing-a-postcss-plugin
+        * https://evilmartians.com/chronicles/postcss-8-plugin-migration
+
+    * postcss-selector-parser
+        * https://github.com/postcss/postcss-selector-parser/blob/HEAD/API.md
+
+    * postcss-media-query-parser
+        * https://github.com/dryoma/postcss-media-query-parser
+
+    * @csstools
+
+        * selector-specificity
+            * https://github.com/csstools/postcss-plugins/blob/main/packages/selector-specificity/src/index.ts
+
+* css
+    * https://developer.mozilla.org/en-US/docs/Web/CSS/Reference
+
+* utils
+
+    * string
+        * https://www.npmjs.com/package/string
--- @athari-recolor/index.js
+++ @athari-recolor/index.js
@@ -1,8 +1,6 @@
 import fs from 'node:fs/promises';
 import { basename } from 'node:path';
 import vm from 'node:vm';
-import postCss, { Comment, Declaration, Rule } from 'postcss';
-import safeParser from 'postcss-safe-parser';
 import enquirer from 'enquirer';
 import JSON5 from 'json5';
 import { format as prettifyCode } from 'prettier';
@@ -12,11 +10,33 @@ import autoPrefixerPlugin from 'autoprefixer';
 import { initializeLinq } from 'linq-to-typescript'
 import { regex, pattern as re } from 'regex'
 import cssColorsNames from 'color-name';
+import S from 'string';
 import monkeyutils from '@athari/monkeyutils'
+const { isString, assignDeep, throwError, withTimeout } = monkeyutils;
 import {
   Command,
   Option as CommandOption,
 } from 'commander';
+import postCss, {
+  Comment as CssComment,
+  Declaration as CssDecl,
+  Rule as CssRule,
+} from 'postcss';
+import cssSafeParser from 'postcss-safe-parser';
+import cssSelectorParser from 'postcss-selector-parser';
+const {
+  isAttribute: isSelAttribute,
+  isCombinator: isSelCombinator,
+  isContainer: isSelContainer,
+  isNode: isSelNode,
+  isPseudo: isSelPseudo,
+  isRoot: isSelRoot,
+  isSelector: isSelSelector,
+} = cssSelectorParser;
+import {
+  selectorSpecificity as getCssSelSpecificity,
+  compare as compareCssSelSpecificity,
+} from '@csstools/selector-specificity'
 import {
   color as parseCssColor,
   serializeRGB as serializeRgb,
@@ -61,8 +81,11 @@ import makeFetchCookie from 'fetch-cookie';
 import { CookieJar, MemoryCookieStore } from 'tough-cookie';
 import NetscapeCookieStore from './tough-cookie-netscape.js';
 
-const { isString, assignDeep, throwError, withTimeout } = monkeyutils;
 initializeLinq();
+S.extendPrototype();
+String.prototype.ellipsis = function (maxLength) {
+  return this.length > maxLength ? this.substring(0, maxLength - 3) + "..." : this;
+};
 
 const downloadTimeout = 30000;
 
@@ -96,9 +119,178 @@ const parseCssCompStr = css => parseCssComp(tokenizeCss({ css }));
 const roundStrNumbers = s => s.replace(/(\d+\.\d+|\d+\.|\.\d+)/g,
   (_, d) => (+d).toFixed(2).replace(/(\.\d*)0+$/, "$1").replace(/\.0+$/, ""));
 
+const mergeSimilarSelectorsPlugin = (opts = {}) => {
+  opts = Object.assign({
+    pseudo: ':is',
+    unsafe: false,
+  }, opts);
+
+  const reIsValue = regex('i')`
+    ^ :
+    ( - ( webkit | moz | ms ) - )?
+    ( is | any | matches ) $
+  `;
+  const defaultPrintHeadWidth = 40;
+  const defaultPrintCssWidth = 80;
+
+  const removeSelectorComments = (root) => {
+    root.walkComments(comment => {
+      console.log(`comment "${comment.value}"`);
+      comment.remove();
+    });
+  };
+
+  const printNodeHead = (node, headWidth = defaultPrintHeadWidth) => (
+    node.value === undefined ? node.type :
+    isSelCombinator(node) ? `"${node.value}"` :
+    isSelAttribute(node) ? `${node.type} "${node.attribute}" ${node.operator}${node.insensitiveFlag} "${node.value}"` :
+    `${node.type} "${node.value}"`
+  ).padRight(headWidth);
+
+  const printNodeHeadFull = (node, headWidth = defaultPrintHeadWidth, cssWidth = defaultPrintCssWidth) =>
+    printNodeHead(node, headWidth) + ` -> ${node.toString().ellipsis(cssWidth)}`;
+
+  const printNode = (root, headWidth = defaultPrintHeadWidth, cssWidth = defaultPrintCssWidth) => {
+    const indentStr = "  ";
+    const printNodeProc = (node, indent = "") => [
+      `${indent}${printNodeHeadFull(node, headWidth, cssWidth)}`,
+      ...isSelContainer(node) ? node.map(n => printNodeProc(n, indent + indentStr)) : [],
+    ].join("\n");
+    return printNodeProc(root);
+  };
+
+  const areNodesEqual = (a, b) => a.toString() == b.toString();
+
+  const areNodeHeadersEqual = (a, b) => {
+    if (a.type != b.type)
+      return false;
+    else if (isSelPseudo(a))
+      return a.value == b.value;
+    else if (!isSelContainer(a))
+      return areNodesEqual(a, b);
+    else
+      throw new Error("Should not be comparing those");
+  };
+
+  const areNodesCompatible = (a, b, specA, specB) => {
+    if (areNodeHeadersEqual(a, b))
+      return true;
+    if (a.type == b.type)
+      return true;
+    specA ??= getCssSelSpecificity(a);
+    specB ??= getCssSelSpecificity(b);
+    if (!compareCssSelSpecificity(specA, specB))
+      return false;
+    // TODO:> Check compat properly
+    return a.type == b.type;
+  };
+
+  class TrieNode {
+    /** @const {string} */
+    type = 'trie'
+    /** @type {TrieVariant[]} */
+    variants = []
+    /** @type {TrieNode[]} */
+    tries = []
+    /** @param {Partial<TrieNode>} init */
+    constructor(init) { Object.assign(this, init) }
+  }
+
+  class TrieVariant {
+    type = 'variant'
+    /** @type {import('postcss-selector-parser').Selector} */
+    selector
+    /** @type {import('postcss-selector-parser').Node} */
+    node
+    /** @type {import('@csstools/selector-specificity').Specificity} */
+    //specificity
+    /** @param {Partial<TrieVariant>} init */
+    constructor(init) { Object.assign(this, init) }
+  }
+
+  const printTrie = (trie) =>
+    JSON5.stringify(trie, {
+      quote: false,
+      space: "  ",
+      replacer: (k, v) => {
+        if (isSelNode(v))
+          return k != 'selector' ? printNodeHeadFull(v) : undefined;
+        if (v instanceof TrieVariant)
+          return printNodeHeadFull(v.node);
+        if (k == 'type' || v instanceof Array && v.length == 0)
+          return undefined;
+        return v;
+      },
+    });
+
+  /** @param {import('postcss-selector-parser').Root} root */
+  const mergeSimilarSelectors = (root) => {
+    const trieRoot = new TrieNode();
+
+    /** 
+     * @param {import('postcss-selector-parser').Node} node
+     * @param {TrieNode} trie */
+    const mergeByPrefix = (node, trie) => {
+      if (isSelRoot(node)) {
+        trie.variants.push(new TrieVariant({ node }));
+        node.each(sel => {
+          mergeByPrefix(sel, trie);
+        });
+      } else if (isSelSelector(node)) {
+        let currentTrie = trie;
+        node.each(part => {
+          let compatTrie = currentTrie.tries.find(t => t.variants.some(v => areNodesCompatible(v.node, part)));
+          if (!compatTrie) {
+            compatTrie = new TrieNode();
+            currentTrie.tries.push(compatTrie);
+          }
+          if (compatTrie.variants.every(v => !areNodesEqual(v.node, part))) {
+            compatTrie.variants.push(new TrieVariant({ selector: node, node: part }));
+          }
+          currentTrie = compatTrie;
+          if (isSelContainer(part))
+            mergeByPrefix(part, compatTrie);
+        })
+      } else if (isSelContainer(node)) {
+        // TODO:> Process container node
+      } else if (isSelPseudo(node)) {
+        // TODO: Pseudo: split :is, chain others
+      } else {
+        throw new Error("unexpected?");
+      }
+    };
+    mergeByPrefix(root, trieRoot);
+    console.log(printTrie(trieRoot));
+  };
+
+  return {
+    postcssPlugin: 'merge-similar-selectors',
+    Rule(rule) {
+      if (rule.selectors.length <= 1)
+        return;
+      cssSelectorParser((root) => {
+        console.log(printTrie(new TrieVariant({ node: root })));
+        removeSelectorComments(root);
+        console.log(printNode(root));
+        mergeSimilarSelectors(root);
+      }).processSync(rule, { lossless: false });
+    }
+  };
+};
+mergeSimilarSelectorsPlugin.postcss = true;
+
 const derandomSelectorPlugin = (opts = {}) => ({
   postcssPlugin: 'derandom-selector',
   Once(css) {
+    opts = Object.assign({
+      classTransforms: [
+        {
+          find: [ 'main=identSingleDash', '--', 'sub=identSingleDash', '--', 'hash=identSingleDash' ],
+          replace: '[class*="{main}--{sub}--"]',
+        },
+      ],
+    }, opts);
+
     // https://www.w3.org/TR/2021/CRD-css-syntax-3-20211224/#token-diagrams
     const r = {};
     r.newLine = re`(
@@ -156,16 +348,6 @@ const derandomSelectorPlugin = (opts = {}) => ({
     )`;
     // TODO: Support declarative derandom replacements
     //console.log(regex('i')`^${identToken}$`);
-    opts = Object.assign({
-      // gradients--sunnyDay-bottom--tRq3J
-      classTransforms: [
-        {
-          find: [ 'main=identSingleDash', '--', 'sub=identSingleDash', '--', 'hash=identSingleDash' ],
-          replace: '[class*="{main}--{sub}--"]',
-        },
-      ],
-    }, opts);
-
     css.walkRules(rule => {
       let didDerandom = false;
 
@@ -246,7 +428,7 @@ const recolorPlugin = (opts = {}) => ({
 
     css.walkAtRules(rule => {
       //console.log(`rule: ${rule.name} = ${rule.params}`);
-      // TODO: Support animating colors with @keyframes
+      // TODO: Support animating colors with @keyframes (parse at-rules with https://github.com/postcss/postcss-at-rule-parser)
       if (
         rule.name == 'media' && reAtRuleMediaDark.test(rule.params) ||
         !reAtRuleMediaNameAllowed.test(rule.name)
@@ -352,14 +534,14 @@ const recolorPlugin = (opts = {}) => ({
 
     if (opts.palette) {
       css.prepend(
-        new Rule({
+        new CssRule({
           selector: ':root',
           nodes: palette.colors
             .orderBy(c => c.count, (a, b) => b - a)
             .thenBy(c => c.colorRgb, (a, b) => a.localeCompare(b))
             .selectMany(c => [
-              new Comment({ text: `color ${c.colorStr} n=${c.count} ${c.colorRgb} ${c.colorOkLch}` }),
-              new Declaration({ prop: c.name, value: c.expr }),
+              new CssComment({ text: `color ${c.colorStr} n=${c.count} ${c.colorRgb} ${c.colorOkLch}` }),
+              new CssDecl({ prop: c.name, value: c.expr }),
             ])
             .toArray(),
         })
@@ -401,7 +583,7 @@ const downloadSiteHtml = async (siteName, site) => {
 
   const addSiteCss = (css) => {
     site.css ??= [];
-    // TODO: > Deduplicate CSS URLs
+    // TODO: Specify CSS source for debugging
     if (css.url && site.css.some(c => c.url == css.url))
       return false;
     site.css.push(css);
@@ -574,9 +756,10 @@ const downloadSiteHtml = async (siteName, site) => {
 
 const recolorCss = async (inputPath, outputPath, opts) => {
   const runPostCss = async (css, plugins) => {
-    const result = await postCss(plugins).process(css, { from: inputPath, parser: safeParser });
+    const result = await postCss(plugins).process(css, { from: inputPath, parser: cssSafeParser });
     for (const message of result.messages)
-      console.log(message);
+      //console.log(message);
+      console.log(`  ${message.plugin}: ${message.type} ${message.node?.toString()}`);
     return result;
   };
   const inputCss = await readTextFile(inputPath);
@@ -585,12 +768,14 @@ const recolorCss = async (inputPath, outputPath, opts) => {
     cssNanoPlugin({
       preset: [
         cssNanoPresetDeault({
-          discardComments: false,
+          //discardComments: false,
         }),
       ],
     }),
   ]);
+  // TODO: Split plugin options
   result = await runPostCss(result.css, [
+    mergeSimilarSelectorsPlugin(opts),
     derandomSelectorPlugin(opts),
     recolorPlugin(opts),
   ]);
