2b96dfd71267356d52053bdcc78cd5663d3cbdd0
Athari <athari.p@gmail.com>
2025-04-03 02:31:42 +0300

    @athari/recolor: (partial) Refont plugin: Added events. Changed to preserve font between parse calls. Added tokenization and stringification of decl props. Added calculation of font size from keywords.

--- @athari-recolor/src/css/cssFonts.ts
+++ @athari-recolor/src/css/cssFonts.ts
@@ -4,13 +4,15 @@ import * as Kw from './cssKeywords.ts';
 import * as Cu from './cssUnits.ts';
 import * as Cv from './cssValues.ts';
 import {
-  Opt,
-  isArray, isString, isObject, isDefined, logError, throwError, deepMerge, fallback,
+  Opt, OptObject,EventName, EventHandler, HasEvents, ReadonlyTuple, IntRange,
+  isArray, isString, isObject, isDefined, isNumber,
+  throwError, deepMerge, fallback, eventGet, eventSet,
 } from '../utils.ts';
+import EventEmitter from 'node:events';
 
 const kw = Kw.kw;
 
-// MARK: Types
+// MARK: Types: Font
 
 export interface Font extends Cv.Shorthand<Kw.Prop.Font.AnyShorthand, AnyShorthand> {
   family?: Opt<Families>;
@@ -62,31 +64,44 @@ export type LineHeight =
   | Cv.KeywordLax<Kw.Font.LineHeight | Kw.GlobalValue>
   | Cv.NumericOptLax<Cu.Length | Cu.Percent>;
 
-export type AnyShorthand = Families | Size | Stretch | Style | Variant | Weight | LineHeight;
+export type AnyShorthand = Cv.AnyValue | Cv.AnyValue[];
 
-export interface ParserOptions {
-  isFontFaceRule?: Opt<boolean>;
+// MARK: Types: Parsing
+
+interface ParserOpts {
+  isFontFaceRule: boolean;
   // TODO: Remove value checks in non-strict mode
-  strict?: Opt<boolean>;
+  strict: boolean;
 }
+
+export interface ParserOptions extends OptObject<ParserOpts> { }
+
+interface ParserEvents {
+  parseError: [ Ct.ParseError, Ct.Token | null ];
+}
+
+type ParserEventsOpts = {
+  [N in keyof ParserEvents]: (...args: ParserEvents[N]) => void;
+}
+
+type Opts = Opt<OptObject<ParserOpts>>;
+type Init = Opt<OptObject<ParserOpts & ParserEventsOpts>>;
 
 // MARK: Types: Utility
 
-type Options = Required<ParserOptions>;
-
 interface CssNode {
-  readonly type: string;
+  readonly type: 'decl' | 'rule' | 'atrule';
 }
 
 type CssAnyNode = CssDecl | CssRule | CssAtRule;
 
-interface CssDecl {
+interface CssDecl extends CssNode {
   readonly type: 'decl';
   get prop(): string
   get value(): string
 }
 
-interface CssRuleOrAtRule {
+interface CssRuleOrAtRule extends CssNode {
   readonly type: 'rule' | 'atrule';
   each(callback: (node: unknown) => void): void;
 }
@@ -100,145 +115,183 @@ interface CssAtRule extends CssRuleOrAtRule {
   get name(): string;
 }
 
-type Source = string | CssDecl;
-
-const defaultOpts: Options = { isFontFaceRule: false, strict: false };
-
 // MARK: Parser
 
-export class Parser extends Cv.Parser {
-  #opts: ParserOptions = defaultOpts;
-  #font!: Font;
+export class Parser extends Cv.Parser implements HasEvents<ParserEvents> {
+  readonly #events = new EventEmitter<ParserEvents>();
+  readonly #defaultOptions: ParserOptions;
+  #opts: ParserOptions;
+  #font: Font;
 
-  constructor() {
+  constructor(init?: Init) {
     super();
-    this.resetFont();
+    const { isFontFaceRule, strict, parseError } = init ?? {};
+    this.#defaultOptions = { isFontFaceRule, strict };
+    this.#opts = this.setOptions({});
+    this.#font = this.resetFont();
+    this.onParseError = parseError ?? null;
   }
 
-  private resetFont() {
-    this.#font = { props: new Map() };
+  get font() { return this.#font }
+
+  resetFont(): Font {
+    return this.#font = { props: new Map() };
   }
 
-  private setFontProp<P extends Exclude<keyof Font, 'props'>>(prop: P, value: Font[P]): Font {
+  private setOptions(opts: Opts): ParserOptions {
+    return this.#opts = deepMerge(null, { isFontFaceRule: false, strict: false }, this.#defaultOptions, opts);
+  }
+
+  private setProp<P extends Exclude<keyof Font, 'props'>>(prop: P, value: Font[P]): Font {
     return Cv.setShorthandProp(this.#font, kw.prop.font, prop, value);
   }
 
-  private parse<T>(source: Source, opts: Opt<ParserOptions>, parse: () => Opt<T>): Opt<T> {
-    const decl: Opt<CssDecl> = isObject(source) && 'type' in source && source.type === 'decl' ? source : undefined;
-    const css: string = decl?.value ?? isString(source) ? <string>source : throwError(`source must be string or Decl, ${typeof(source)} received`);
-    this.#opts = deepMerge(null, defaultOpts, opts);
-    this.p = Ct.tokenizer(css);
-    this.p.onParseError = ex => logError(ex, `Failed to parse ${decl?.prop ?? 'a font property'}`);
-    return parse();
+  on(name: EventName<ParserEvents>, handler: EventHandler<ParserEvents, typeof name>): this {
+    return this.#events.on(name, handler), this;
   }
 
-  // MARK: Parser: Interface
-
-  parseFont(source: Source, opts?: Opt<ParserOptions>): Opt<Font> {
-    this.resetFont();
-    return this.parse(source, opts, () => this.consumeFont());
+  off(name: EventName<ParserEvents>, handler: EventHandler<ParserEvents, typeof name>): this {
+    return this.#events.off(name, handler), this;
   }
 
-  parseFontFamily(source: Source, opts?: Opt<ParserOptions>): Opt<Families> {
-    return this.parse(source, opts, () => this.consumeFontFamilyList());
+  once(name: EventName<ParserEvents>, handler: EventHandler<ParserEvents, typeof name>): this {
+    return this.#events.once(name, handler), this;
   }
 
-  parseFontSize(source: Source, opts?: Opt<ParserOptions>): Opt<Size> {
-    return this.parse(source, opts, () => this.consumeFontSize());
+  get onParseError() { return eventGet(this.#events, 'parseError'); }
+  set onParseError(v) { eventSet(this.#events, 'parseError', v); }
+
+  parseFont(source: CssDecl  | string, opts?: Opts): Opt<Font> {
+    return this.parseProc(source, opts, () => this.consumeFont());
   }
 
-  parseFontStretch(source: Source, opts?: Opt<ParserOptions>): Opt<Stretch> {
-    return this.parse(source, opts, () => this.consumeFontStretch());
+  parseFontFamily(source: CssDecl  | string, opts?: Opts): Opt<Families> {
+    return this.parseProc(source, opts, () => this.consumeFontFamilyList());
   }
 
-  parseFontStyle(source: Source, opts?: Opt<ParserOptions>): Opt<Style> {
-    return this.parse(source, opts, () => this.consumeFontStyle());
+  parseFontFaceFontFamily(source: CssDecl  | string, opts?: Opts): Opt<Families> {
+    return this.parseProc(source, opts, () => this.consumeNonGenericFontFamilyList());
+  }
+
+  parseFontSize(source: CssDecl  | string, opts?: Opts): Opt<Size> {
+    return this.parseProc(source, opts, () => this.consumeFontSize());
+  }
+
+  parseFontStretch(source: CssDecl  | string, opts?: Opts): Opt<Stretch> {
+    return this.parseProc(source, opts, () => this.consumeFontStretch());
+  }
+
+  parseFontStyle(source: CssDecl  | string, opts?: Opts): Opt<Style> {
+    return this.parseProc(source, opts, () => this.consumeFontStyle());
   }
 
   // TODO: Parse full font variant
-  parseFontVariant(source: Source, opts: Opt<ParserOptions> = undefined): Opt<Variant> {
-    return this.parse(source, opts, () => this.consumeFontVariantCSS21());
+  parseFontVariant(source: CssDecl  | string, opts: Opts = undefined): Opt<Variant> {
+    return this.parseProc(source, opts, () => this.consumeFontVariantCSS21());
   }
 
-  parseFontWeight(source: Source, opts?: Opt<ParserOptions>): Opt<Weight> {
-    return this.parse(source, opts, () => this.consumeFontWeight());
+  parseFontWeight(source: CssDecl  | string, opts?: Opts): Opt<Weight> {
+    return this.parseProc(source, opts, () => this.consumeFontWeight());
   }
 
-  parseLineHeight(source: Source, opts?: Opt<ParserOptions>): Opt<LineHeight> {
-    return this.parse(source, opts, () => this.consumeLineHeight());
+  parseLineHeight(source: CssDecl  | string, opts?: Opts): Opt<LineHeight> {
+    return this.parseProc(source, opts, () => this.consumeLineHeight());
   }
 
-  parseDecl(value: string, prop: string, opts?: Opt<ParserOptions>): Opt<Font>;
-  parseDecl(decl: CssDecl, opts?: Opt<ParserOptions>): Opt<Font>;
-  parseDecl(a0: Source, a1?: string | ParserOptions, a2?: Opt<ParserOptions>): Opt<Font> {
+  private parseProc<T>(source: CssDecl  | string, opts: Opts, parse: () => Opt<T>): Opt<T> {
+    const decl: Opt<CssDecl> = isObject(source) && 'type' in source && source.type === 'decl' ? source : undefined;
+    const value: string = decl?.value ?? isString(source) ? <string>source : throwError(`source must be string or Decl, ${typeof(source)} received`);
+    this.setOptions(opts);
+    this.p = Ct.tokenizer(value);
+    this.p.onParseError = (ex) => this.#events.emit('parseError', ex, this.p.lastErrorToken ?? null);
+    return parse();
+  }
+
+  parseDeclValue(value: string, prop: string, opts?: Opts): Opt<Font>;
+  parseDeclValue(decl: CssDecl, opts?: Opts): Opt<Font>;
+  parseDeclValue(a0: CssDecl  | string, a1?: string | Opts, a2?: Opts): Opt<Font> {
     const [ source, prop, opts ] =
       isString(a0) && isString(a1) ? [ a0, a1, a2 ] :
       isObject(a0) && !isString(a1) ? [ a0.value, a0.prop, a1 ] :
       throwError("Incorrect arguments");
-    this.resetFont();
-    return this.parseDeclProc(source, prop, opts);
+    return this.parseDeclValueProc(source, prop, opts);
   }
 
-  private parseDeclProc(source: string, prop: string, opts: Opt<ParserOptions>): Opt<Font> {
+  private parseDeclValueProc(source: string, prop: string, opts: Opts): Opt<Font> {
     if (Kw.equals(prop, kw.prop.font.shorthand))
-      return this.parse(source, opts, () => this.consumeFont());
+      return this.parseProc(source, opts, () => this.consumeFont());
 
     if (Kw.equals(prop, kw.prop.font.shorthandSub)) {
       switch (prop.toLowerCase()) {
-        case kw.prop.font.family: return this.setFontProp('family', this.parseFontFamily(source, opts));
-        case kw.prop.font.size: return this.setFontProp('size', this.parseFontSize(source, opts));
-        case kw.prop.font.stretch: return this.setFontProp('stretch', this.parseFontStretch(source, opts));
-        case kw.prop.font.style: return this.setFontProp('style', this.parseFontStyle(source, opts));
-        case kw.prop.font.variant: return this.setFontProp('variant', this.parseFontVariant(source, opts));
-        case kw.prop.font.weight: return this.setFontProp('weight', this.parseFontWeight(source, opts));
-        case kw.prop.font.lineHeight: return this.setFontProp('lineHeight', this.parseLineHeight(source, opts));
-        default: assert.fail();
+        case kw.prop.font.family:
+          return this.setProp('family', this.#opts.isFontFaceRule
+            ? this.parseFontFaceFontFamily(source, opts)
+            : this.parseFontFamily(source, opts));
+        case kw.prop.font.size:
+          return this.setProp('size', this.parseFontSize(source, opts));
+        case kw.prop.font.stretch:
+          return this.setProp('stretch', this.parseFontStretch(source, opts));
+        case kw.prop.font.style:
+          return this.setProp('style', this.parseFontStyle(source, opts));
+        case kw.prop.font.variant:
+          return this.setProp('variant', this.parseFontVariant(source, opts));
+        case kw.prop.font.weight:
+          return this.setProp('weight', this.parseFontWeight(source, opts));
+        case kw.prop.font.lineHeight:
+          return this.setProp('lineHeight', this.parseLineHeight(source, opts));
+        default:
+          assert.fail();
       }
     }
     throwError(`A font property expected, got ${prop}`);
   }
 
-  parseRule(rule: CssRule, opts?: Opt<ParserOptions>) {
-    return this.#font.props.size > 0 ? this.parseRuleOrAtRuleProc(rule, opts) : undefined;
+  parseRule(rule: CssRule, opts?: Opts): Opt<Font> {
+    return this.parseRuleOrAtRuleProc(rule, opts);
   }
 
-  parseFontFaceAtRule(atRule: CssAtRule, opts?: Opt<ParserOptions>) {
+  parseFontFaceAtRule(atRule: CssAtRule, opts?: Opts): Opt<Font> {
     if (!Kw.equals(atRule.name, kw.atRule.fontFace))
-      throwError(`@font-face at-rule expected, got @${atRule.name}`);
+      throwError(`A @font-face at-rule expected, got @${atRule.name}`);
     return this.parseRuleOrAtRuleProc(atRule, { ...opts, isFontFaceRule: true });
   }
 
-  private parseRuleOrAtRuleProc(source: CssRuleOrAtRule, opts?: Opt<ParserOptions>): Opt<Font> {
-    this.resetFont();
+  private parseRuleOrAtRuleProc(source: CssRuleOrAtRule, opts?: Opts): Opt<Font> {
     source.each((child: unknown) => {
-      const decl  = child as CssAnyNode;
-      if (decl.type !== 'decl')
+      const decl = child as CssAnyNode;
+      if (decl.type !== 'decl' || !Kw.equals(decl.prop, kw.prop.font.all))
         return;
-      if (!Kw.equals(decl.prop, kw.prop.font.all))
-        return;
-      this.parseDeclProc(decl.value, decl.prop, opts);
+      this.parseDeclValueProc(decl.value, decl.prop, opts);
     });
-    return this.#font;
+    return this.#font.props.size > 0 ? this.#font : undefined;
   }
 
   *tokenizeFont(font: Font): ArrayIterator<Ct.Token> {
-    for (const [ prop, value ] of font.props.entries()) {
-      if (prop === 'line-height')
-        yield Ct.delim('/');
-      if (!isArray(value))
-        yield* Cv.toTokens(value);
-      else {
-        for (let i = 0; i < value.length; i++) {
-          if (i > 0)
-            yield Ct.token(Ct.Type.Comma);
-          yield* Cv.toTokens(value[i]!);
-        }
-      }
-    }
+    // TODO: Make sure order is correct (for fonts modified manually)
+    for (const [ prop, value ] of font.props.entries())
+      yield* this.tokenizeDeclValueProc(value, prop, true);
+  }
+
+  *tokenizeDeclValue(value: AnyShorthand, prop: string): ArrayIterator<Ct.Token> {
+    if (!Kw.equals(prop, kw.prop.font.shorthandSub))
+      throwError(`A font property expected, got '${prop}'`);
+    yield* this.tokenizeDeclValueProc(value, prop, false);
+  }
+
+  private *tokenizeDeclValueProc(value: AnyShorthand, prop: string, shorthand: boolean): ArrayIterator<Ct.Token> {
+    if (shorthand && Kw.equals(prop, kw.prop.font.lineHeight))
+      yield Ct.delim('/');
+    yield* isArray(value)
+      ? Cv.tokenizeList(value, Ct.token(Ct.Type.Comma))
+      : Cv.tokenize(value);
   }
 
   stringifyFont(font: Font): string {
     return Ct.stringify(this.tokenizeFont(font));
+  }
+
+  stringifyDeclValue(value: AnyShorthand, prop: string): string {
+    return Ct.stringify(this.tokenizeDeclValue(value, prop));
   }
 
   // MARK: Parser: Parsing
@@ -255,7 +308,7 @@ export class Parser extends Cv.Parser {
     const fontSize = this.consumeFontSize();
     if (!isDefined(fontSize) || this.p.isEof)
       return;
-    this.setFontProp('size', fontSize);
+    this.setProp('size', fontSize);
 
     const slashRaws = this.consumeSlashWithRaws();
     if (isDefined(slashRaws)) {
@@ -263,13 +316,13 @@ export class Parser extends Cv.Parser {
       if (!isDefined(lineHeight))
         return;
       Cv.setRawProp(lineHeight, 'before', slashRaws);
-      this.setFontProp('lineHeight', lineHeight);
+      this.setProp('lineHeight', lineHeight);
     }
 
     const fontFamily = this.consumeFontFamilyList();
     if (!isDefined(fontFamily))
       return;
-    this.setFontProp('family', fontFamily);
+    this.setProp('family', fontFamily);
 
     return this.#font;
   }
@@ -277,7 +330,7 @@ export class Parser extends Cv.Parser {
   private consumeSystemFont(): Opt<Font> {
     const ct = this.p.peek();
     if (Ct.isIdent(ct))
-      this.setFontProp('family', [ Cv.fromTokenPeek(ct, this.p) ]);
+      this.setProp('family', [ Cv.fromTokenPeek(ct, this.p) ]);
     return this.#font;
   }
 
@@ -294,7 +347,7 @@ export class Parser extends Cv.Parser {
       if (!isDefined(this.#font.style) && Kw.equals(ident, kw.font.style.all)) {
         const fontStyle = this.consumeFontStyle();
         if (isDefined(fontStyle)) {
-          this.setFontProp('style', fontStyle);
+          this.setProp('style', fontStyle);
           continue;
         }
         return;
@@ -302,21 +355,21 @@ export class Parser extends Cv.Parser {
       if (!isDefined(this.#font.variant) && Kw.equals(ident, kw.font.variantCss21)) {
         const fontVariant = this.consumeFontVariantCSS21();
         if (isDefined(fontVariant)) {
-          this.setFontProp('variant', fontVariant);
+          this.setProp('variant', fontVariant);
           continue;
         }
       }
       if (!isDefined(this.#font.weight)) {
         const fontWeight = this.consumeFontWeight();
         if (isDefined(fontWeight)) {
-          this.setFontProp('weight', fontWeight);
+          this.setProp('weight', fontWeight);
           continue;
         }
       }
       if (!isDefined(this.#font.stretch)) {
         const fontStretch = this.consumeFontStretchKeywordOnly();
         if (isDefined(fontStretch)) {
-          this.setFontProp('stretch', fontStretch);
+          this.setProp('stretch', fontStretch);
           continue;
         }
       }
@@ -472,4 +525,79 @@ export class Parser extends Cv.Parser {
       return;
     return sb.join("");
   }
+}
+
+// MARK: Size calc
+
+// Based on https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/core/css/font_size_functions.cc
+
+interface SizeOpts {
+  readonly quirksMode: boolean;
+  readonly defaultFontSize: number; // normal=16 monospace=13
+}
+
+export type SizeOptions = OptObject<SizeOpts>;
+
+const sizeKeyBegin = 9;
+const sizeKeyEnd = 17;
+
+type SizeKey = IntRange<typeof sizeKeyBegin, typeof sizeKeyEnd>;
+type SizeFactors = ReadonlyTuple<number, 8>;
+type SizeTable = { [N in SizeKey]: SizeFactors };
+
+// CSS font-size = legacy HTML <font size>
+// xxs=(0) xs=1 s=2 m=3 l=4 xl=5 xxl=6 (xxxl)=7
+// m = user pref
+const sizeTable = new class {
+  readonly quirks: SizeTable = {
+    9: [ 9, 9, 9, 9, 11, 14, 18, 28 ],
+    10: [ 9, 9, 9, 10, 12, 15, 20, 31 ],
+    11: [ 9, 9, 9, 11, 13, 17, 22, 34 ],
+    12: [ 9, 9, 10, 12, 14, 18, 24, 37 ],
+    13: [ 9, 9, 10, 13, 16, 20, 26, 40 ],
+    14: [ 9, 9, 11, 14, 17, 21, 28, 42 ],
+    15: [ 9, 10, 12, 15, 17, 23, 30, 45 ],
+    16: [ 9, 10, 13, 16, 18, 24, 32, 48 ],
+  };
+  readonly strict: SizeTable = {
+    9: [ 9, 9, 9, 9, 11, 14, 18, 27 ],
+    10: [ 9, 9, 9, 10, 12, 15, 20, 30 ],
+    11: [ 9, 9, 10, 11, 13, 17, 22, 33 ],
+    12: [ 9, 9, 10, 12, 14, 18, 24, 36 ],
+    13: [ 9, 10, 12, 13, 16, 20, 26, 39 ],
+    14: [ 9, 10, 12, 14, 17, 21, 28, 42 ],
+    15: [ 9, 10, 13, 15, 18, 23, 30, 45 ],
+    16: [ 9, 10, 13, 16, 18, 24, 32, 48 ],
+  };
+  readonly factors: SizeFactors = [
+    0.60, 0.75, 0.89, 1.0, 1.2, 1.5, 2.0, 3.0,
+  ];
+  get(quirksMode: boolean): SizeTable {
+    return quirksMode ? this.quirks : this.strict;
+  }
+};
+
+/** Convert CSS absolute `font-size` keyword `xx-small` | ... | `xxx-large` ({@link Kw.Font.SizeAbsolute}) to pixel font size. */
+export function getPixelSizeForKeyword(keyword: string, opts?: Opt<SizeOptions>): Opt<number> {
+  return getSizeProc(Kw.index(keyword, kw.font.size.absolute), opts);
+}
+
+/** Convert N from legacy `<font size=N>` to pixel font size. */
+export function getPixelSizeForFontTag(size: number, opts?: Opt<SizeOptions>): Opt<number> {
+  return getSizeProc(size + 1, opts);
+}
+
+function getSizeProc(col: number, opts?: Opt<SizeOptions>): Opt<number> {
+  if (!isNumber(col) || col < 0 || col >= 8)
+    return;
+  const opt: SizeOpts = deepMerge(null, { quirksMode: false, defaultFontSize: 16 }, opts);
+  const row = getSizeTableRow(opt);
+  return row !== -1
+    ? sizeTable.get(opt.quirksMode)[row][col]
+    : sizeTable.factors[col]! * opt.defaultFontSize;
+}
+
+function getSizeTableRow(opts: SizeOpts): SizeKey | -1 {
+  const mediumSize = opts.defaultFontSize;
+  return mediumSize >= sizeKeyBegin && mediumSize < sizeKeyEnd ? mediumSize as SizeKey : -1;
 }
